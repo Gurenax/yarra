48dce00db8983b8bf4837907d018c78a4a048587
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-06 13:13:07 +1100

    Modify init

--- api/models/init.js
+++ api/models/init.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise
 
 // Connect to our local database
 mongoose
-  .connect('mongodb://localhost/storms', { useMongoClient: true })
+  .connect('mongodb://localhost/yarra', { useMongoClient: true })
   .then(() => {
     console.log('Successfully connected to database')
   })
