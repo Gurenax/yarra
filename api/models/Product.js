84ada4c13b3461290112267a440d29c7a80f150d
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-09 10:35:14 +1100

    API: Update Product to include categories

--- api/models/Product.js
+++ api/models/Product.js
@@ -1,8 +1,10 @@
 const mongoose = require('./init')
+const Schema = mongoose.Schema
 
 const Product = mongoose.model('Product', {
   brandName: String, // e.g. Holden
-  name: String // e.g. Commodore
+  name: String, // e.g. Commodore
+  categories: [{ type: Schema.ObjectId, ref: 'Category', default: [] }]
 })
 
 module.exports = Product
