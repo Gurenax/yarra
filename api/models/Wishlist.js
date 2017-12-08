a374ccacdef84f6d5ef011c3366629a6df474f85
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-08 12:50:18 +1100

    API: Update wishlist model

--- api/models/Wishlist.js
+++ api/models/Wishlist.js
@@ -5,7 +5,7 @@ const wishListSchema = new Schema({   // One to Many relationship
   // Belongs to user
   user: { type: Schema.ObjectId, ref: 'User', unique: true }, // One User
   // Has many products
-  product: [{ type: Schema.ObjectId, ref: 'Product' }]  // Many products
+  products: [{ type: Schema.ObjectId, ref: 'Product' }]  // Many products
 })
 
 const Wishlist = mongoose.model('Wishlist', wishListSchema)
