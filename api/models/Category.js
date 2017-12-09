a19de8cffee61ba05d0ed9bea7dfc2701157b076
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-12-09 11:10:49 +1100

    API: Add unique constraint to Category.name

--- api/models/Category.js
+++ api/models/Category.js
@@ -2,7 +2,7 @@ const mongoose = require('./init')
 const Schema = mongoose.Schema
 
 const Category = mongoose.model('Category', {
-  name: String, // e.g. Cars
+  name: { type: String, unique: true }, // e.g. Cars
   products: [{ type: Schema.ObjectId, ref: 'Product', default: [] }]
 })
 
